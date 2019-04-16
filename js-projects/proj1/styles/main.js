dfac510680a1c2a1dcf7555fd71f8519db1481bc
Eunice Baiden <ebaiden@techie.com>
2019-04-15 22:10:38 -0400

    April 15

---
+++ js-projects/proj1/styles/main.js
@@ -0,0 +1,11 @@
+document.getElementById("insert").innerHTML = "Say Whatt <br> Like whhhhuuuuuuu <h2>Say what a minute now</h2>";
+
+var x = 10;
+document.getElementById("inserted").innerHTML = x;
+
+var name = "Bob";
+var age = 15;
+
+document.getElementById("concat").innerHTML = `hello       world and ${age}`;
+
+document.getElementById("quora").innerHTML = name + ' is ' + age;
